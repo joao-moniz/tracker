7ba138a4115ff7a12b559a4f9f92567b1b08211f
João Moniz Alves e Urzedo <joao-moniz@hotmail.com>
2023-09-08 15:13:27 -0300

    Log fix

--- server.js
+++ server.js
@@ -10,37 +10,11 @@ server.createServer ({
 server.on ('track', function (gps) {
     console.log('Event Track' + gps);
 });
-server.on ('data', function (raw) {
-    console.log ('Event data: '+ raw);
-  })
 
-  server.on ('listening', function (listen) {
-    console.log(" Event Listening"+listen);
-  });
-  server.on ('connection', function (socket) {
-    console.log ('Connection from '+ socket.remoteAddress);
-  });
-
-  server.on ('disconnect', function (socket) {
-    console.log ('Disconnected device '+ socket.remoteAddress);
-  });
-
-  server.on ('timeout', function (socket) {
-    console.log ('Time-out from '+ socket.remoteAddress);
-  });
-
-  server.on ('fail', function (err) {
-    console.log ("Event fail " + err);
-  });
-
-  server.on ('error', function (err) {
-    console.log ("Event error "+err);
-  });
-
-  server.on ('log', function (name, value) {
-    console.log ('Log: ' + name);
-    console.log (value);
-  });
+server.on ('log', function (name, value) {
+console.log ('Log: ' + name);
+console.log (value);
+});
 
 
 console.log("Servidor Online");
